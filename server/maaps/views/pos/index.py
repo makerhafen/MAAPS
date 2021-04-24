c7419219505bff58eed9e6fadf172de2289186d1
Dirk Makerhafen <office@makerhafen.de>
2021-04-24 17:25:50 +0200

    updates

--- server/maaps/views/pos/index.py
+++ server/maaps/views/pos/index.py
@@ -4,16 +4,14 @@ from django.shortcuts import redirect
 from maaps.views.functions.session import get_profile_from_post, get_profile_from_session
 
 def pos__index(request):
-    profile = get_profile_from_session(request)
-    admin_profile = None
-    if profile.user.is_staff:
-        admin_profile = profile
+    admin_profile = get_profile_from_session(request)
     error = None
     if admin_profile is None:
-        admin_profile, error = get_profile_from_post(request)
-        if admin_profile is not None:
-            request.session["profile_id"] = admin_profile.id
-
+        profile, error = get_profile_from_post(request)
+        if profile is not None and profile.user.is_staff:
+            request.session["profile_id"] = profile.id
+            admin_profile = profile
+            
     template = loader.get_template('pos/index.html')
     return HttpResponse(template.render({
         "admin_profile": admin_profile,
